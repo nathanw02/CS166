885d2996aab6e401929577cc006d6aeb5e01fb52
Nathan Wong <nathanw1510@gmail.com>
2024-03-03 10:17:31 -0800

    path

--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -1,32 +1,32 @@
 COPY Users
-FROM 'users.csv'
+FROM '/home/csmajs/nwong063/CS166/data/users.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE users_userID_seq RESTART 101;
 
 COPY Store
-FROM 'stores.csv'
+FROM '/home/csmajs/nwong063/CS166/data/stores.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Product
-FROM 'products.csv'
+FROM '/home/csmajs/nwong063/CS166/data/products.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Warehouse
-FROM 'warehouse.csv'
+FROM '/home/csmajs/nwong063/CS166/data/warehouse.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Orders
-FROM 'orders.csv'
+FROM '/home/csmajs/nwong063/CS166/data/orders.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE orders_orderNumber_seq RESTART 501;
 
 
 COPY ProductSupplyRequests
-FROM 'productSupplyRequests.csv'
+FROM '/home/csmajs/nwong063/CS166/data/productSupplyRequests.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productsupplyrequests_requestNumber_seq RESTART 11;
 
 COPY ProductUpdates
-FROM 'productUpdates.csv'
+FROM '/home/csmajs/nwong063/CS166/data/productUpdates.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productupdates_updateNumber_seq RESTART 51;
